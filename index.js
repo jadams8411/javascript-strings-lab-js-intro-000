c5a5c5d1e1274f827df5036501b56be4e654d869
Jeremy Adams <jadams8411@gmail.com>
2018-08-15 19:21:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,3 @@ var greeting = 'Hello, everybody!'
 var greetSpecialGuest = "Hello" + specialGuest + "!"
  var greetSpecialGuest = "Hello, " + specialGuest + "!"
 var topic = "Let's talk about space."
-
-"
